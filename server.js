f9c07602c47c8e99ab27a4678012fbabc5804fb0
leolecrado <34286846+leolecrado@users.noreply.github.com>
2020-04-24 09:36:10 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const people = require('./people.json')
 const app = express()
 
 app.set('view engine', 'pug')
-app.use(express.static(__dirname + '/public'))
+app.use(express.static('./public'))
 
 
 const server = app.listen(443, () => {
