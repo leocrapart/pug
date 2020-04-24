9e1066d85558ebecfa1e9cdd0b16f10050a92d79
leolecrado <34286846+leolecrado@users.noreply.github.com>
2020-04-24 09:12:27 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const people = require('./people.json')
+const app = express()
+
+app.set('view engine', 'pug')
+app.use(express.static(__dirname + '/public'))
+
+
+const server = app.listen(443, () => {
+    console.log(`Express running => PORT ${server.address().port}`)
+})
+
+app.get('/', (req, res) => {
+    res.render("index", {
+        title: "Homepage",
+        people: people.profiles
+    })
+})
+
+app.get("/profile", (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id)
+    res.render("profile", {
+        title: `About ${person.firstname} ${person.lastname}`,
+        person,
+    })
+})
+
+
+
